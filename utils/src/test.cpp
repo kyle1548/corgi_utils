d567ce25a7dbf251213c90dc67e006941cfaa1be
kyle1548 <kyle9296@gmail.com>
2024-11-15 01:36:16 +0800

    finished "fitted_coefficient.hpp"

---
+++ utils/src/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include "../include/fitted_coefficient.hpp"
+
+
+int main() {
+
+    std::cout << E_poly[0](10) << std::endl;
+
+    return 0;
+}
